66528eea7e2ee811fb06c5874802a75b115fcbab
Elisa Vaccaro <erv230@nyu.edu>
2017-10-05 00:17:52 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+  "hello" => "hi",
+  "to" => "2",
+  "too" => "2",
+  "two" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+def word_substituter(tweet_str)
+shortened = []
+words = dictionary.keys
+tweet_str.split.each do |word|
+  if(words.contains(word))
+    shortened.push(dictionary[word])
+  else
+    shortened.push(word)
+  end
+end
+shortened
+end
